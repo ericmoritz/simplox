5d963d251881c1f6c48d1d7f5e40d441f6842a7f
Eric Moritz <eric@themoritzfamily.com>
2013-11-21 13:34:41 -0500

    troubleshooting heroku

--- apps/simplox/src/simplox_dummy_handler.erl
+++ apps/simplox/src/simplox_dummy_handler.erl
@@ -47,14 +47,19 @@ content_types_accepted(Req, State) ->
     ], Req, State}.
 
 
-echo_parser(Req2, State=#state{boundary=Boundary}) ->
+echo_parser(Req, State=#state{boundary=Boundary}) ->
+    Req2 = case cowboy_req:qs_val(<<"add-t-e">>, Req) of
+	       {undefined, R} ->
+		   R;
+	       {_, R} ->
+		   cowboy_req:set_resp_header(
+		     <<"transfer-encoding">>, <<"identity">>, R)
+	   end,
     Req3 = cowboy_req:set_resp_body(dummy_data(),
-				    cowboy_req:set_resp_header(
-				      <<"transfer-encoding">>, <<"identity">>,
 				      cowboy_req:set_resp_header(
 				      <<"content-type">>,
 				      <<"multipart/mixed; boundary=", Boundary/binary>>,
-				      Req2))),
+				      Req2)),
     {true, Req3, State}.
 
 resource_exists(Req, State) ->
